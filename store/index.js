74f08eebdbdb840e8f171bdd483afb4789df0042
umisuna <llluna@qq.com>
2018-03-09 11:09:55 +0800

    fix user data path

--- store/index.js
+++ store/index.js
@@ -1,3 +1,6 @@
 const path = require('path')
+const { app } = require('electron')
 
-exports.USER_DATA_PATH = path.resolve(process.env.APPDATA, 'blhxfy')
+exports.USER_DATA_PATH = app 
+  ? path.resolve(app.getPath('userData'))
+  : path.resolve(process.cwd(), 'userData')
