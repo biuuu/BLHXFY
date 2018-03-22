59368a1140e53024ca3e7004f93c73b67fb90168
cocona <llluna@qq.com>
2018-03-22 15:48:41 +0800

    update icon

--- store/index.js
+++ store/index.js
@@ -1,6 +1,12 @@
 const path = require('path')
 const { app } = require('electron')
 
+const cwd = process.cwd()
+
 exports.USER_DATA_PATH = app 
   ? path.resolve(app.getPath('userData'))
-  : path.resolve(process.cwd(), 'userData')
+  : path.resolve(cwd, 'userData')
+
+exports.STATIC_PATH = app
+  ? path.resolve(app.getPath('userData'), './static/')
+  : path.resolve(cwd, './data/static/')
