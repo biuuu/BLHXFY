96ce1546537074dbc0818491be2e2e6c6a180aed
biuuu <blhx@danmu9.com>
2018-05-03 12:08:57 +0800

    update

--- main/gameWindow.js
+++ main/gameWindow.js
@@ -1,13 +1,14 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
 const { session } = require('electron')
+const { default: installExtension } = require('electron-devtools-installer')
 
 module.exports = () => {
+  installExtension('fgpokpknehglcioijejfeebigdnbnokj')
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err))
   const ses = session.fromPartition('persist:blhxfy')
-  ses.setUserAgent('Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36')
-  ses.setProxy({
-    proxyRules: 'http=127.0.0.1:8001'
-  }, () => {})
+  // ses.setUserAgent('Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3071.115 Safari/537.36')
   ipcMain.on('show-win-game', () => {
     gameWin = new BrowserWindow({
       width: 360, height: 590,
@@ -21,22 +22,7 @@ module.exports = () => {
         plugins: true
       }
     })
-    // gameWin.webContents.on('new-window', function(e, url) {
-    //   e.preventDefault()
-    //   const win = new BrowserWindow({
-    //     width: 360, height: 590,
-    //     icon: path.resolve(__dirname, '../assets/icon.ico'),
-    //     frame: false,
-    //     webPreferences: {
-    //       preload: path.resolve(__dirname, './preload.js'),
-    //       nodeIntegration: false,
-    //       session: ses,
-    //       plugins: true
-    //     }
-    //   })
-    //   win.loadURL(url)
-    //   e.newGuest = win
-    // })
-    gameWin.loadURL('http://gbf.game.mbga.jp')
+
+    gameWin.loadURL('http://game.granbluefantasy.jp')
   })
 }
--- main/preload.js
+++ main/preload.js
@@ -4,9 +4,16 @@ window.addEventListener('load', () => {
   ::-webkit-scrollbar {
     display: none;
   }
-  .prt-head-current {
+  #prt-treasure-slider, .prt-head-current {
     -webkit-app-region: drag;
+  }
+  footer#treasure-footer .cnt-treasure-footer {
+    height: 40px;
   }
   `
   document.head.appendChild(style)
+  const footer = document.createElement('footer')
+  footer.id = 'treasure-footer'
+  footer.innerHTML = '<div class="cnt-treasure-footer"><div class="btn-treasure-footer-back"></div>						<div class="btn-treasure-footer-reload"></div>						<div class="btn-treasure-footer-mypage" data-href="mypage"></div>	<div id="prt-treasure-slider" class="prt-treasure-slider"><ul class="lis-treasures" id="treasure-list" data-treasure-max="9"></ul></div></div><div id="treasure-pop"></div>'
+  document.getElementById('wrapper').appendChild(footer)
 })
