f503dd631fa5c52ee017dd35c691b5c50ed659fa
biuuu <blhx@danmu9.com>
2018-05-09 17:07:30 +0800

    update

--- main/gameWindow.js
+++ main/gameWindow.js
@@ -4,16 +4,16 @@ const { session } = require('electron')
 const { default: installExtension } = require('electron-devtools-installer')
 
 module.exports = () => {
-  installExtension('fgpokpknehglcioijejfeebigdnbnokj')
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err))
+  // installExtension('fgpokpknehglcioijejfeebigdnbnokj')
+  //   .then((name) => console.log(`Added Extension:  ${name}`))
+  //   .catch((err) => console.log('An error occurred: ', err))
   const ses = session.fromPartition('persist:blhxfy')
-  // ses.setUserAgent('Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3071.115 Safari/537.36')
+  ses.setUserAgent('Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3071.115 Safari/537.36')
   ipcMain.on('show-win-game', () => {
     gameWin = new BrowserWindow({
-      width: 360, height: 590,
-      icon: path.resolve(__dirname, '../assets/icon.ico'),
-      frame: false,
+      width: 390, height: 640,
+      icon: path.resolve(__dirname, '../assets/game.ico'),
+      // frame: false,
       alwaysOnTop: true,
       webPreferences: {
         preload: path.resolve(__dirname, './preload.js'),
@@ -23,6 +23,6 @@ module.exports = () => {
       }
     })
 
-    gameWin.loadURL('http://game.granbluefantasy.jp')
+    gameWin.loadURL('http://gbf.game.mbga.jp')
   })
 }
