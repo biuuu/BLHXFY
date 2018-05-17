f1b8e0476c62faa7920ab0c32e511bf3249cb11c
biuuu <blhx@danmu9.com>
2018-05-17 15:54:05 +0800

    change the game script Game.ua.isPcPlatform always be true

--- main/preload.js
+++ main/preload.js
@@ -112,4 +112,7 @@ window.addEventListener('load', () => {
   footer.id = 'treasure-footer'
   footer.innerHTML = '<div class="cnt-treasure-footer"><div class="btn-treasure-footer-back"></div>						<div class="btn-treasure-footer-reload"></div>						<div class="btn-treasure-footer-mypage" data-href="mypage"></div>	<div id="prt-treasure-slider" class="prt-treasure-slider"><ul class="lis-treasures" id="treasure-list" data-treasure-max="9"></ul></div></div><div id="treasure-pop"></div>'
   document.getElementById('wrapper').appendChild(footer)
+  Game.ua.isPcPlatform = function () {
+    return true
+  }
 })
