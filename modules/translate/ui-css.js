c3911e9be3a0236796af916036c811b8839dc1ce
biuuu <llluna@qq.com>
2018-06-24 21:58:10 +0800

    update pac rules (#67)

--- modules/translate/ui-css.js
+++ modules/translate/ui-css.js
@@ -1,7 +1,9 @@
+const ip = require('ip')
 
+const localIp = ip.address()
 
 const transUI = (responseDetail, CONFIG) => {
-  const cssLink = `http://127.0.0.1:${CONFIG.staticPort}/style/BLHXFY.css?lang=${CONFIG.lang}`
+  const cssLink = `http://${localIp}:${CONFIG.staticPort}/style/BLHXFY.css?lang=${CONFIG.lang}`
   let body = responseDetail.response.body.toString()
   body = body.replace(/<head>([\s\S]+?)<\/head>/, `<head>$1<link rel="stylesheet" type="text/css" media="screen" href="${cssLink}" /></head>`)
   responseDetail.response.body = body
