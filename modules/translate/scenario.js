950af829035cd96508dbe0811c704d335f65d51b
biuuu <llluna@qq.com>
2018-08-21 09:25:36 +0800

    npc name add scenario scope (#119)

--- modules/translate/scenario.js
+++ modules/translate/scenario.js
@@ -18,25 +18,47 @@ if (CONFIG.transService === 'baidu') transApi = baiduTrans
 const WORDS_LIMIT = 4500
 const txtKeys = ['chapter_name', 'synopsis', 'detail', 'sel1_txt', 'sel2_txt', 'sel3_txt', 'sel4_txt']
 
-const replaceChar = (key, item, map) => {
+const getNameTrans = (name, map, scenarioName) => {
+  const item = map.get(name)
+  if (item) {
+    let existScenario = ''
+    if (item.scenarios.length) {
+      for (let sName of item.scenarios) {
+        if (scenarioName.indexOf(sName) !== -1) {
+          existScenario = sName
+          break
+        }
+      }
+    }
+    const result = { trans: item.trans, noun: item.noun }
+    if (existScenario) {
+      result.trans = item[existScenario].trans
+      result.noun = item[existScenario].noun
+    }
+    return result.trans
+  }
+  return null
+}
+
+const replaceChar = (key, item, map, scenarioName) => {
   const name = item[key]
   if (name && name !== 'null' && name !== '???' && name !== '？？？') {
-    let trans = map.get(name)
+    let trans = getNameTrans(name, map, scenarioName)
     let _name = name
 
     if (/\s?[\?？0-9０－９]{1,2}$/.test(name)) {
       // name with number or symbol
       const nameRst = name.match(/(.+?)\s?([\?？0-9０－９]{1,2})$/)
-      const _trans = map.get(nameRst[1])
+      const _trans = getNameTrans(nameRst[1], map, scenarioName)
       _name = nameRst[1]
       if (_trans) trans = `${_trans}${nameRst[2]}`
     } else if (/'s\sVoice$/.test(name)) {
       let nmKey = name.slice(0, name.length - 8)
-      const _trans = map.get(nmKey)
+      const _trans = getNameTrans(nmKey, map, scenarioName)
       if (_trans) trans = `${_trans}的声音`
     } else if (/の声$/.test(name)) {
       let nmKey = name.slice(0, name.length - 2)
-      const _trans = map.get(nmKey)
+      const _trans = getNameTrans(nmKey, map, scenarioName)
       if (_trans) trans = `${_trans}的声音`
     }
 
@@ -160,9 +182,9 @@ const transStart = async (data, uid, pathname) => {
   let names = []
   data.forEach((item, index) => {
     let name1, name2, name3
-    name1 = replaceChar('charcter1_name', item, nameMap)
-    name2 = replaceChar('charcter2_name', item, nameMap)
-    name3 = replaceChar('charcter3_name', item, nameMap)
+    name1 = replaceChar('charcter1_name', item, nameMap, scenarioName)
+    name2 = replaceChar('charcter2_name', item, nameMap, scenarioName)
+    name3 = replaceChar('charcter3_name', item, nameMap, scenarioName)
     name1 && !names.includes(name1) && names.push(name1)
     name2 && !names.includes(name2) && names.push(name2)
     name3 && !names.includes(name3) && names.push(name3)
