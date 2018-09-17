7fbd63edc145b92e8f698b1f140467be0ec50151
biuuu <llluna@qq.com>
2018-09-17 16:43:08 +0800

    fix replace keywords error

--- extension/utils/sortKeywords.js
+++ extension/utils/sortKeywords.js
@@ -1,6 +1,6 @@
 const sortKeywords = (list, key = 'name') => {
   return list.sort((prev, next) => {
-    if (next[key] === prev[key]) {
+    if (!next[key] || !prev[key] || next[key] === prev[key]) {
       return 0
     } else if (next[key].includes(prev[key])) {
       return 1
