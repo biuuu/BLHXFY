5131243d109683fc86ab016636e888571ad95c6d
biuuu <llluna@qq.com>
2018-09-18 14:11:29 +0800

    update

--- extension/modules/content-html.js
+++ extension/modules/content-html.js
@@ -1,4 +1,5 @@
 import getCommHtmlData from '../store/common-html'
+import getArchiveData from '../store/archive'
 
 const replaceHTML = async (html, pathname) => {
   let _html = html
@@ -20,6 +21,22 @@ const replaceHTML = async (html, pathname) => {
   return _html
 }
 
+const replaceArchive = async (html) => {
+  let _html = html
+  const htmlMap = await getArchiveData()
+  for (let [text, item] of htmlMap.entries()) {
+    for (let i = 0; i < item.times; i++) {
+      let newHtml = _html.replace(text, item.trans)
+      if (newHtml !== _html) {
+        _html = newHtml
+      } else {
+        break
+      }
+    }
+  }
+  return _html
+}
+
 export default async function transHTML(data, pathname) {
   if (!data.data) return data
   let html
@@ -30,7 +47,11 @@ export default async function transHTML(data, pathname) {
   }
   console.info(pathname)
   console.log(html)
-  html = await replaceHTML(html, pathname)
+  if (pathname.includes('/archive/content/library/')) {
+    html = await replaceArchive(html)
+  } else {
+    html = await replaceHTML(html, pathname)
+  }
   data.data = encodeURIComponent(html)
   return data
 }
