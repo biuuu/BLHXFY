38e42bf8338b8b6f61bce4494fd808c4d6d514ac
biuuu <llluna@qq.com>
2018-09-19 14:27:19 +0800

    update

--- extension/translate.js
+++ extension/translate.js
@@ -6,9 +6,11 @@ import transJobSkill from './modules/skill-job'
 import transHTML from './modules/content-html'
 import transTownInfo from './modules/town-info'
 import transIslandInfo from './modules/island-info'
+import showVoiceSub from './modules/voice-sub'
 import getUserName from './store/name-user'
 
 const apiHosts = ['game.granbluefantasy.jp', 'gbf.game.mbga.jp']
+const voiceHosts = ['game-a5.granbluefantasy.jp', 'gbf.game-a5.mbga.jp']
 
 export default async function translate(state) {
   const uri = URI(state.url)
@@ -21,30 +23,36 @@ export default async function translate(state) {
   } catch (err) {
     isJSON = false
   }
-  if (apiHosts.indexOf(hostname) === -1) return
-  if (pathname.includes('scenario')) {
-    data = await transScenario(data, pathname)
-  } else if (pathname.includes('/content/')) {
-    if (pathname.includes('/profile/content/index/')) {
-      getUserName(data)
+  if (apiHosts.indexOf(hostname) !== -1) {
+    if (pathname.includes('scenario')) {
+      data = await transScenario(data, pathname)
+    } else if (pathname.includes('/content/')) {
+      if (pathname.includes('/profile/content/index/')) {
+        getUserName(data)
+      }
+      data = await transLangMsg(data, pathname)
+      data = await transHTML(data, pathname)
+      if (pathname.includes('/user/content/index')) {
+        data = await transTownInfo(data, pathname)
+      }
+    } else if (pathname.includes('/npc/npc/') || pathname.includes('/archive/npc_detail')) {
+      data = await transNpcSkill(data, pathname)
+    } else if (
+      pathname.includes('/party_ability_subaction/') ||
+      pathname.includes('/party/job/') ||
+      pathname.includes('/party/ability_list/') ||
+      pathname.includes('/party/job_info/')) {
+      data = await transJobSkill(data, pathname)
+    } else if (pathname.includes('/island/init')) {
+      data = await transIslandInfo(data, pathname)
+    } else if (pathname.includes('/rest/sound/mypage_voice')) {
+      await showVoiceSub(data, pathname, 'list')
+    } else {
+      return
     }
-    data = await transLangMsg(data, pathname)
-    data = await transHTML(data, pathname)
-    if (pathname.includes('/user/content/index')) {
-      data = await transTownInfo(data, pathname)
-    }
-  } else if (pathname.includes('/npc/npc/') || pathname.includes('/archive/npc_detail')) {
-    data = await transNpcSkill(data, pathname)
-  } else if (
-  pathname.includes('/party_ability_subaction/') ||
-  pathname.includes('/party/job/') ||
-  pathname.includes('/party/ability_list/') ||
-  pathname.includes('/party/job_info/')) {
-    data = await transJobSkill(data, pathname)
-  } else if (pathname.includes('/island/init')) {
-    data = await transIslandInfo(data, pathname)
   } else {
     return
   }
+
   state.result = isJSON ? JSON.stringify(data) : data
 }
