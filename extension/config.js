1cc042df1c6a9a63920400191b3d3fefc80b9703
biuuu <llluna@qq.com>
2018-09-20 11:16:58 +0800

    auto get userName (#214)

--- extension/config.js
+++ extension/config.js
@@ -2,7 +2,7 @@ const config = {
   origin: 'https://blhx.danmu9.com',
   apiHosts: ['game.granbluefantasy.jp', 'gbf.game.mbga.jp'],
   hash: '',
-  userName: '姬塔'
+  userName: ''
 }
 
 export default config
