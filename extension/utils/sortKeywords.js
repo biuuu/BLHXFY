de8ed6e2e50562436e5572abbedd4b088ec695a4
biuuu <llluna@qq.com>
2018-10-09 14:54:46 +0800

    fix sortkeywords

--- extension/utils/sortKeywords.js
+++ extension/utils/sortKeywords.js
@@ -1,12 +1,13 @@
 const sortKeywords = (list, key = 'name') => {
   return list.sort((prev, next) => {
-    if (!next[key] || !prev[key] || next[key] === prev[key]) {
-      return 0
-    } else if (next[key].includes(prev[key])) {
-      return 1
-    } else {
-      return -1
+    if (next[key] && prev[key]) {
+      if (next[key].includes(prev[key])) {
+        return 1
+      } else if (prev[key].includes(next[key])) {
+        return -1
+      }
     }
+    return 0
   })
 }
 
