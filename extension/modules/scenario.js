fa240b4edfe28f7c2dc47cb31f0ffa50486480eb
biuuu <llluna@qq.com>
2018-10-15 09:55:46 +0800

    fix name with number

--- extension/modules/scenario.js
+++ extension/modules/scenario.js
@@ -74,9 +74,9 @@ const replaceChar = (key, item, map, scenarioName) => {
     let trans = getNameTrans(name, map, scenarioName)
     let _name = name
 
-    if (/\s?[\?？0-9０－９]{1,2}$/.test(name)) {
+    if (/\s?[\?？0-9０-９]{1,2}$/.test(name)) {
       // name with number or symbol
-      const nameRst = name.match(/(.+?)\s?([\?？0-9０－９]{1,2})$/)
+      const nameRst = name.match(/(.+?)\s?([\?？0-9０-９]{1,2})$/)
       const _trans = getNameTrans(nameRst[1], map, scenarioName)
       _name = nameRst[1]
       if (_trans) trans = `${_trans}${nameRst[2]}`
