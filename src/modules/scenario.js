ae25d3492f72a638479f01cecf3a225a7dc6d2fa
cocona <llluna@qq.com>
2018-10-25 20:25:06 +0800

    replace / in scenario name

--- src/modules/scenario.js
+++ src/modules/scenario.js
@@ -115,7 +115,7 @@ const transStart = async (data, pathname) => {
   if (pathRst[1].includes('birthday')) {
     let rst = pathname.match(/\/[^/]*?scenario.*?\/(scene.+)$/)
     if (!rst || !rst[1]) return data
-    sNameTemp = rst[1]
+    sNameTemp = rst[1].replace(/\//g, '_')
   }
   insertToolHtml()
   const scenarioName = sNameTemp
