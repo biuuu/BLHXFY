95666a83da149ed333491cd78ad8ba119068dfba
cocona <llluna@qq.com>
2018-11-18 21:46:39 +0800

    fix wrong word

--- src/utils/fix-url-search-params.js
+++ src/utils/fix-url-search-params.js
@@ -1,5 +1,5 @@
 
-try { if (new URLSearchParams({q:'+'}).get('+') !== '+' || new URLSearchParams('q=%2B').get('q') !== '+') throw {}; }
+try { if (new URLSearchParams({q:'+'}).get('q') !== '+' || new URLSearchParams('q=%2B').get('q') !== '+') throw {}; }
 catch (error) {
   window.URLSearchParams = void 0;
 }
