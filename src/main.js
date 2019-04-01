5b28d1634d73489b69089f3235c50aa6c73930d0
biuuu <10892119+biuuu@users.noreply.github.com>
2019-04-01 12:42:31 +0800

    about conflict in two scripts

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import injectXHR from './xhr'
 import eventMessage from './utils/eventMessage'
 
 const main = () => {
-  if (window.blhxfy) return
+  const time = sessionStorage.getItem('blhxfy:startTime') || 0
+  const now = Date.now()
+  if (now - time < 1000) return
+  sessionStorage.setItem('blhxfy:startTime', now)
   eventMessage()
   injectXHR()
 }
