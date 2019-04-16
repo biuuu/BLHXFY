00c20e6983a217c52a0b662beccbd2c490e5404f
biuuu <10892119+biuuu@users.noreply.github.com>
2019-04-16 09:17:47 +0800

    stop translate setting btn hint

--- src/setting/settingBtnText.js
+++ src/setting/settingBtnText.js
@@ -1,5 +1,5 @@
-window.addEventListener('DOMContentLoaded', function () {
-  setTimeout(() => {
-    if (jQuery) jQuery('.prt-global-ext .prt-config-balloon').html('感觉卡顿的时候，可以通过调整设定来改善')
-  }, 1000)
-})
+// window.addEventListener('DOMContentLoaded', function () {
+//   setTimeout(() => {
+//     if (jQuery) jQuery('.prt-global-ext .prt-config-balloon').html('感觉卡顿的时候，可以通过调整设定来改善')
+//   }, 1000)
+// })
