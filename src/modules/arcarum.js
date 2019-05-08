35e61516d85ad1f6cffe80df1737ff74ad02ba4f
Lacia <10892119+biuuu@users.noreply.github.com>
2019-05-08 22:59:56 +0800

    fix arcarum trans

--- src/modules/arcarum.js
+++ src/modules/arcarum.js
@@ -26,19 +26,35 @@ const replaceListText = (key, list) => {
   }
 }
 
+const transDivisionList = (list) => {
+  for (let key in list) {
+    const item = list[key]
+    replaceText('text', item)
+    replaceListText('message', item.gatepost_list)
+    replaceListText('message', item.chest_list)
+    for (let qlk in item.quest_list) {
+      replaceText('quest_name', item.quest_list[qlk])
+      replaceText('chapter_name', item.quest_list[qlk])
+    }
+  }
+}
+
 const transArcarum = async (data) => {
   arcarumMap = await getArcarumData()
   replaceText('group_name', data.condition)
   replaceText('name', data.stage)
   if (data.map && data.map.division_list) {
-    for (let key in data.map.division_list) {
-      const item = data.map.division_list[key]
-      replaceText('text', item)
-      replaceListText('message', item.gatepost_list)
-      replaceListText('message', item.chest_list)
-      for (let qlk in item.quest_list) {
-        replaceText('quest_name', item.quest_list[qlk])
-        replaceText('chapter_name', item.quest_list[qlk])
+    transDivisionList(data.map.division_list)
+  }
+  if (data.notice_effect) {
+    for (let key in data.notice_effect) {
+      let obj = data.notice_effect[key]
+      if (obj && obj.maps) {
+        obj.maps.forEach(map => {
+          if (map.division_list) {
+            transDivisionList(map.division_list)
+          }
+        })
       }
     }
   }
