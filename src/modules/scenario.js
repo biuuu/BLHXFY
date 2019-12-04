814ba64e1f9929b9783ec839bfe0e8911c7262a2
mio <10892119+biuuu@users.noreply.github.com>
2019-12-04 15:56:33 +0800

    update caiyun out of limit hint

--- src/modules/scenario.js
+++ src/modules/scenario.js
@@ -277,8 +277,7 @@ const transStart = async (data, pathname) => {
         obj[info.type] = transList[index] || ''
         if (!transNotice && info.index === startIndex && info.type === 'detail' && transList.length > 0) {
           if (transList[0] === 'caiyunoutoflimit') {
-            obj[info.type] = `<span style="color:#fd8484;font-size:10px;">彩云小译超出使用次数，请尝试登录彩云账号后再使用。可以通过点击右上角Log按钮，然后点击链接打开登录页面：</span><a style="color:#62dccb;font-size:10px;" href="http://www.caiyunapp.com/user/login/" target="_blank">彩云用户登录</a>
-              <span style="font-size:10px;color:#999">如果登录后仍然看到这个提示，则可能需要成为彩云付费会员后才能继续使用。</span>`
+            obj[info.type] = `<span style="color:#fd8484;font-size:10px;">彩云小译超出使用次数，请尝试登录彩云账号后再使用。可以通过点击右上角Log按钮，然后点击链接打开登录页面：</span><a style="color:#62dccb;font-size:10px;" href="http://www.caiyunapp.com/user/login/" target="_blank">彩云用户登录</a>`
           } else {
             obj[info.type] = `<a href="${apiData[1]}" target="_blank" class="autotrans-hint-blhxfy ${config.transApi}-blhxfy"> </a>${obj[info.type]}`
           }
