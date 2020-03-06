cb896160f8a7882a5cf823750aa0dc2d5ab5f364
mio <10892119+biuuu@users.noreply.github.com>
2020-03-06 09:29:44 +0800

    Update: url rewrite setting

--- script/deploy.js
+++ script/deploy.js
@@ -6,7 +6,7 @@ const start = async () => {
     await fse.outputFile('./dist/CNAME', 'blhx.danmu9.com')
   }
   await fse.outputFile('./dist/blhxfy/game-config.js', `document.write('<script src="' + Game.jsUri + '/config.js?lyria"></script>')
-document.write('<script src="https://blhx.danmu9.com/blhxfy/extension.ios.user.js"></script>')`)
+document.write('<script src="https://cdn.jsdelivr.net/gh/biuuu/BLHXFY@gh-pages/blhxfy/extension.ios.user.js?t=' + Math.floor(Date.now()/21600000) + '"></script>')`)
 
   if (process.env.TRAVIS) {
     console.log('travis')
