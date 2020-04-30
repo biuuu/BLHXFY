28a24dcb25a185dd3d5fbc8eb3bf492552b8194d
mio <10892119+biuuu@users.noreply.github.com>
2020-04-30 10:02:06 +0800

    移除多余的文件

--- src/setting/settingBtnText.js
+++
@@ -1,5 +0,0 @@
-// window.addEventListener('DOMContentLoaded', function () {
-//   setTimeout(() => {
-//     if (jQuery) jQuery('.prt-global-ext .prt-config-balloon').html('感觉卡顿的时候，可以通过调整设定来改善')
-//   }, 1000)
-// })
--- src/utils/replaceTurn.js
+++
@@ -1,9 +0,0 @@
-export default function (str) {
-  return str.replace('ターン', '回合')
-    .replace('turns', '回合')
-    .replace('turn', '回合')
-    .replace('Cooldown', '使用间隔')
-    .replace('使用間隔', '使用间隔')
-    .replace('初回召喚', '初次召唤')
-    .replace('後', '后')
-}
